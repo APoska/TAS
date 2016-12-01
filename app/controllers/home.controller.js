3c1d12fc698d12b433459b8f9e20310c362f4ac6
APoska <aposka31@gmail.com>
2016-12-01 20:17:17 +0100

    k

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -5,3 +5,18 @@ angular.module('app')
     $state.go('login');
   };
 })
+
+.controller('monthCalCtrl', function($scope){
+	var firstDay = new Date(2016, 11, 1);
+	var daynumb = firstDay.getDay();
+	var lastDay = new Date(2016, 12, 0);
+	var daycount = lastDay.getDate();
+	for(i=1; i<=daycount; i++){
+		var count = i+daynumb-1;
+		var c = count.toString();
+		var x = 'date'+c;
+		var day = new Date(firstDay.getFullYear(), firstDay.getMonth(), i);
+		console.log(x);
+		$scope.x = day;
+	}
+})
