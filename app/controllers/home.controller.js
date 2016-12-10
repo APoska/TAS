a183818900b168ad24d70a66f713fff29e233694
APoska <aposka31@gmail.com>
2016-12-10 10:29:51 +0100

    calgenerating

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -9,4 +9,40 @@ angular.module('app')
   $scope.tasks = function() {
     $state.go('tasks');
   };
+	$scope.weekdays=[
+  		{"name": "Monday", "shortName":"MON"},
+  		{"name": "Tuesday", "shortName":"TUE"},
+  		{"name": "Wednesday", "shortName":"WED"},
+  		{"name": "Thursday", "shortName":"THU"},
+  		{"name": "Friday", "shortName":"FRI"},
+  		{"name": "Saturday", "shortName":"SAT"},
+  		{"name": "Sunday", "shortName":"SUN"}
+  	]
+
+  	
+	var firstDay = new Date(2016, 11, 1);
+	var daynumb = firstDay.getDay();
+	var lastDay = new Date(2016, 12, 0);
+	var daycount = lastDay.getDate();
+	var days = [];
+	if(daynumb===0)
+		daynumb=7;
+	for(i=1; i<daynumb; i++){
+		var foo = new Date(firstDay.getFullYear(), firstDay.getMonth()-1, 8)
+		days.push({day: foo});
+	}
+	for(i=1; i<=daycount; i++){
+		var foo = new Date(firstDay.getFullYear(), firstDay.getMonth(), i);
+		days.push({day: foo});
+	}
+	var num = 43-daynumb-daycount;
+	for(i=1; i<=num; i++){
+		var foo = new Date(firstDay.getFullYear(), firstDay.getMonth(), 9);
+		days.push({day: foo});
+	}
+	$scope.days=days;
+
+  
+
+
 })
