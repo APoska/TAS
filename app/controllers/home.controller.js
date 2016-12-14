0a8f6e92eba01597bb5cafbf2920c622e1c93b2b
Aleksander Pośka <s407287@labs.wmi.amu.edu.pl>
2016-12-14 10:58:18 +0100

    monthpicker

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -11,8 +11,21 @@ angular.module('app')
 	};
 
 	var date = new Date();
+	var yy = date.getFullYear();var mm = date.getMonth();var dd = date.getDate();
 
-	var yy = date.getFullYear();	var mm = date.getMonth();	var dd = date.getDate();
+	var months = [];
+	months[0] = "Styczeń";
+	months[1] = "Luty";
+	months[2] = "Marzec";
+	months[3] = "Kwiecień";
+	months[4] = "Maj";
+	months[5] = "Czerwiec";
+	months[6] = "Lipiec";
+	months[7] = "Sierpień";
+	months[8] = "Wrzesień";
+	months[9] = "Październik";
+	months[10] = "Listopad";
+	months[11] = "Grudzień";
 
 
 	var firstDay = new Date(yy, mm, 1);
@@ -29,17 +42,16 @@ angular.module('app')
 		if(daynumb===0)
 			daynumb=7;
 		for(i=1; i<daynumb; i++){
-			var foo = new Date(firstDay.getFullYear(), firstDay.getMonth()-1, 8)
-			days.push({day: foo});
+			var foo = new Date(firstDay.getFullYear(), firstDay.getMonth()-1, 8); days.push({day: foo});
 		}
 		for(i=1; i<=daycount; i++){
-			var foo = new Date(firstDay.getFullYear(), firstDay.getMonth(), i);
-			days.push({day: foo});
+			var foo = new Date(firstDay.getFullYear(), firstDay.getMonth(), i);	days.push({day: foo});
 		}
 		var num = 43-daynumb-daycount;
+		if(num >= 7) num = num - 7;
+		
 		for(i=1; i<=num; i++){
-			var foo = new Date(firstDay.getFullYear(), firstDay.getMonth(), 9);
-			days.push({day: foo});
+			var foo = new Date(firstDay.getFullYear(), firstDay.getMonth(), i);	days.push({day: foo});
 		}
 		$scope.days=days;
 	};
@@ -47,26 +59,26 @@ angular.module('app')
 	$scope.nextMonth = function() {
 		mm++;
 		if(mm===12){
-			mm=0;
-			yy++;
+			mm=0; yy++;
 		}
 		days=[];
 		generateCalendar();
+		currentDate = months[mm] + '\n' + yy;$scope.month = currentDate;
 	};
 
 	$scope.prevMonth = function() {
 		mm--;
 		if(mm===-1){
-			mm=11;
-			yy--;
+			mm=11; yy--;
 		}
-		console.log(yy); console.log(mm);
 		days = [];
 		generateCalendar();
-		console.log(days);
+		currentDate = months[mm] + '\n' + yy;$scope.month = currentDate;
 	};
 	generateCalendar();
 	
+	currentDate = months[mm] + '\n' + yy;
+	$scope.month = currentDate;
 
 	$scope.weekdays=[
   		{"name": "Monday", "shortName":"MON"},
