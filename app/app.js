9b74b026e7bdb62c7b49dbce2d34c118907b0551
Radoslaw Wojtysiak <worad94@gmail.com>
2017-01-02 20:43:16 +0100

    Workflow prepare

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ui.router', 'isteven-multi-select'])
+angular.module('app', ['ui.router'])
   .config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('login', {
