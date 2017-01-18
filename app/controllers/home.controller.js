c5e6c7ddf26e127b4e2a117dad37c2dbd4d435af
Aleksander Pośka <aposka31@gmail.com>
2017-01-18 13:37:47 +0100

    add task and meeting number in home

--- app/controllers/home.controller.js
+++ app/controllers/home.controller.js
@@ -1,5 +1,5 @@
 angular.module('app')
-.controller('HomeCtrl', function($scope, UserService, $state) {
+.controller('HomeCtrl', function($scope, UserService, TasksService, MeetingsService, $state) {
 
 	$scope.logout = function() {
 		UserService.logout();
@@ -14,10 +14,27 @@ angular.module('app')
 		$state.go('meetings');
 	};
 
+	var authToken = window.localStorage.getItem("yourTokenKey");
+	var allTasks = [];
+	var allMeetings = [];
+   	UserService.getUserDetails(authToken).then(function(user){				
+				// Get my meeting list
+				MeetingsService.getMeetingDetails(user).then(function(meetings){
+					$scope.Meetings = meetings;
+					allMeetings = meetings;
+				});
+				// Get my tasks list
+				TasksService.getTaskDetails(user).then(function(tasks){
+						$scope.Tasks = tasks;
+						allTasks = tasks;
+						generateCalendar();
+				});
+	   });
+
 	var date = new Date();
 	var yy = date.getFullYear(); var mm = date.getMonth(); var dd = date.getDate(); var currentMonth = mm; var currentYear = yy;
 
-	var months = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 	var firstDay = new Date(yy, mm, 1);
 	var daynumb = firstDay.getDay();
@@ -33,11 +50,37 @@ angular.module('app')
 		var daycount = lastDay.getDate();
 		if(daynumb===0)
 			daynumb=7;
+		//array of days from other month to hide
 		for(i=1; i<daynumb; i++){
 			var foo = new Date(firstDay.getFullYear(), firstDay.getMonth()-1, 8); prevMonthDays.push({day: foo});
 		}
+		//array of days from actual month
 		for(i=1; i<=daycount; i++){
 			var foo = new Date(firstDay.getFullYear(), firstDay.getMonth(), i);	days.push({day: foo});
+			var y=firstDay.getFullYear(); var m = firstDay.getMonth()+1;
+			if(m<10){
+				m = '0' + m;
+			}
+			var taskCount = 0;
+			var meetCount = 0;
+			if(i<10){
+				i = '0' + i;
+			}
+			var actualDate = y + '-' + m + '-' + i;
+			//counting tasks in actual day
+			for(j=0; j<allTasks.length; j++){
+				if(allTasks[j].startDate == actualDate){
+					taskCount++;
+				}
+			}
+			for(j=0; j<allMeetings.length; j++){
+				if(allMeetings[j].startDate == actualDate){
+					meetCount++;
+				}
+			}
+			days[days.length-1].meetCount = meetCount; //add number of meetings to day obj
+			days[days.length-1].taskCount = taskCount; //add number of task to day obj	
+			//add 
 			if(i==dd && mm == currentMonth && yy == currentYear){
 				days[days.length-1].today = true;
 			}
@@ -45,11 +88,18 @@ angular.module('app')
 				days[days.length-1].today = false;
 			}
 		}
-		console.log(days);
 		$scope.prevMonthDays = prevMonthDays;
 		$scope.days=days;
+		console.log(days);
 	};
-
+	//next month
+	$scope.actualMonth = function() {
+		mm = currentMonth; yy = currentYear;
+		days=[];prevMonthDays=[];
+			generateCalendar();
+		currentDate = months[mm] + '\n' + yy; $scope.month = currentDate;
+	};
+	//previous month
 	$scope.nextMonth = function() {
 		mm++;
 		if(mm===12){
@@ -59,7 +109,7 @@ angular.module('app')
 				generateCalendar();
 		currentDate = months[mm] + '\n' + yy; $scope.month = currentDate;
 	};
-
+	//back to actual month
 	$scope.prevMonth = function() {
 		mm--;
 		if(mm===-1){
@@ -69,14 +119,9 @@ angular.module('app')
 		generateCalendar();
 		currentDate = months[mm] + '\n' + yy; $scope.month = currentDate;
 	};
-	generateCalendar();
 	
 	currentDate = months[mm] + '\n' + yy;
 	$scope.month = currentDate;
 
-	$scope.weekdays=[
-  		{"name": "Monday", "shortName":"MON"}, {"name": "Tuesday", "shortName":"TUE"}, {"name": "Wednesday", "shortName":"WED"},
-  		{"name": "Thursday", "shortName":"THU"}, {"name": "Friday", "shortName":"FRI"}, {"name": "Saturday", "shortName":"SAT"},
-  		{"name": "Sunday", "shortName":"SUN"}
-  	]
+	$scope.weekdays=[{"shortName":"MON"}, {"shortName":"TUE"}, {"shortName":"WED"}, {"shortName":"THU"}, {"shortName":"FRI"}, {"shortName":"SAT"}, {"shortName":"SUN"}]
 })
