93052994c2cfdb0fb64c09c4c71ff76e4cb24e14
Radoslaw Wojtysiak <worad94@gmail.com>
2017-01-25 10:26:17 +0100

    Fix to find

--- routes/meetings.route.js
+++ routes/meetings.route.js
@@ -9,8 +9,7 @@ router.route('/meetings')
 		var creator = req.query.user;
 		
 		if(typeof creator != "undefined"){
-			Meeting.find({user : creator}, function(err,meetings){
-				if(err){
+			Meeting.find({$or:[{user : creator},{$and:[{'guests.id':{$in:[creator]}},{'guests.flag':{$in:['pending','accepted']}}]}]}, function(err,meetings){				if(err){
 					res.status(400);
 					return res.send(err);
 				}else{
